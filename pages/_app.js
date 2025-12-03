e5f9a419f4c57b23b201616895abd2dd35020ba0
jbrady1985 <89276626+jbrady1985@users.noreply.github.com>
2025-12-03 15:31:54 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/globals.css';
-
+// pages/_app.js
+import "@/styles/globals.css"; // keep if you have global styles; otherwise remove
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
